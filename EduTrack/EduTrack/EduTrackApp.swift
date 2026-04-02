27cb99f8245f007b7e8482889ebabee34b44e4b9
Ch-Anish24 <anishchatla1106@gmail.com>
2026-04-03 00:43:13 +0530

    Add files via upload

---
+++ EduTrack/EduTrack/EduTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  EduTrackApp.swift
+//  EduTrack
+//
+//  Created by anish chatla on 28/03/26.
+//
+
+import SwiftUI
+
+@main
+struct EduTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
